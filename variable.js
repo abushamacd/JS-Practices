30521f7e0bfdf6649e2d92fcfa88f59aca50b0a5
abushamacd <assiddik001@gmail.com>
2026-03-06 03:25:14 +0600

    variable

---
+++ variable.js
@@ -0,0 +1,19 @@
+var age = 30;
+console.log(age);
+
+var name = "Abu Shama";
+console.log(name);
+
+var my_address = "CD"; // snake case
+var myAddress = "CD"; // camel case
+var MyAddress = "CD"; // pascal case
+
+/* 
+Rules of variable declear
+============================
+1. Do not use reserve keyword
+2. Do not start with number
+3. No space in variable name
+4. No qoute like - "name"
+5. variable name is case sensitive
+*/
