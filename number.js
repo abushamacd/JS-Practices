236a8c95837143abb2c762e2445e0e2c3ed7452e
abushamacd <assiddik001@gmail.com>
2026-03-06 03:52:57 +0600

    number

---
+++ number.js
@@ -0,0 +1,28 @@
+let intiger = 2;
+let float = 1.203;
+var stringToNumber = parseInt("22");
+
+let floatString = float.toFixed(2);
+// console.log(typeof floatString);
+
+var toFixedToNumber = parseFloat(floatString);
+// console.log(typeof toFixedToNumber);
+
+let age = 22;
+
+// age = age + 2;
+// shorthand
+age += 2;
+console.log(age);
+
+/*
+ Operators in js
+ ---------------
+ + = addition
+ - = substruction
+ * = multipication
+ / = dividation
+ % = modulas
+ ** = root
+
+*/
