a2497ecc8949f8edfa86cd1ab7f9bfdea3f74b77
abushamacd <assiddik001@gmail.com>
2026-03-06 09:48:16 +0600

    comparrision

---
+++ comparision.js
@@ -0,0 +1,19 @@
+/*
+Comparison Operator
+=====================
+greater: >
+less: <
+equal: = 
+greater then or equal: >= 
+less then or equal: <=
+not equal: !=
+and: &&
+or: ||
+*/
+
+console.log(5 > 10);
+console.log(5 < 10);
+console.log(5 == 10);
+console.log(5 != 10);
+console.log(10 != "10");
+console.log(10 !== "10");
