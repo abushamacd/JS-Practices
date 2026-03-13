77ddb9e96885f98e7b77157a26b86293b39bd33f
abushamacd <assiddik001@gmail.com>
2026-03-13 11:11:59 +0600

    unit convater

---
+++ unit_convater.js
@@ -0,0 +1,15 @@
+// inch to feet
+function inchToFeet(inch) {
+  var feet = inch / 12;
+  return feet;
+}
+
+console.log(inchToFeet(21));
+
+// miles to kilometre
+function milesToKilometre(miles) {
+  var kilometre = miles * 1.60934;
+  return kilometre;
+}
+
+console.log(milesToKilometre(5));
