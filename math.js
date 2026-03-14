9c8109f413cc3db4c1dc22a1c59a6ba98d2a67f9
abushamacd <assiddik001@gmail.com>
2026-03-14 08:26:50 +0600

    math function

---
+++ math.js
@@ -0,0 +1,8 @@
+console.log(Math.max(23, 23, 253, 456, 1, 45, 6, 756, 34));
+console.log(Math.min(23, 23, 253, 456, 1, 45, 6, 756, 34));
+console.log(Math.abs(-5));
+console.log(Math.pow(2, 3));
+console.log(Math.sqrt(16));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.8));
+console.log(Math.round(Math.random() * 100));
